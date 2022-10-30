0b54d6e3072ebf1419bd958cc7a710a1b2521010
Sosnus <sosnus15@gmail.com>
2022-10-31 00:43:48 +0100

    add new case adc with voltage

--- 03-adc.py
+++ 03-adc.py
@@ -1,3 +1,5 @@
+### V1 simple
+"""
 import machine
 import time
 from machine import Pin, PWM
@@ -11,3 +13,21 @@ while True:
     # write value from ADC to PWM
     pwm.duty_u16(a)
     time.sleep_ms(10)
+"""
+
+### V2 voltage
+# """
+import machine
+import time
+from machine import Pin, PWM
+adc = machine.ADC(1) # ADC0 = GPIO 27
+vref = 3.3
+div = 2.0
+while True:
+    # read ADC
+    rawAdc = adc.read_u16()
+    voltage = div * vref * (rawAdc/65535.0)
+    # print(voltage)
+    print("U= " +str(voltage)+" V")
+    time.sleep_ms(10)
+# """
