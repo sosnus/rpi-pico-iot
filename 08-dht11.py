1aa60ac2aa76cc7faa2c2150d7c1b50e5d30b32c
Sosnus <sosnus15@gmail.com>
2024-04-14 18:45:04 +0200

    add dht11

---
+++ 08-dht11.py
@@ -0,0 +1,13 @@
+from machine import Pin, I2C
+import utime as time
+from dht import DHT11
+# , InvalidChecksum
+
+while True:
+    time.sleep(5)
+    pin = Pin(20, Pin.OUT, Pin.PULL_DOWN)
+    sensor = DHT11(pin)
+    t  = (sensor.temperature)
+    h = (sensor.humidity)
+    print("Temperature: {}".format(sensor.temperature))
+    print("Humidity: {}".format(sensor.humidity))
